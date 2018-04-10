82a789844eb5accdd4d5ce057ddb930bd24fdffb
magda sekulska <magdasekulska@wp.pl>
2018-04-10 18:10:02 +0200

    writer service

--- src/main/java/com/sda/service/WriterService.java
+++ src/main/java/com/sda/service/WriterService.java
@@ -1,12 +1,64 @@
 package com.sda.service;
 
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
 public class WriterService {
     public String write(String name){
-        String nameToDisplay = name == null ?"my friend" : name;
-        return "Hello, " + nameToDisplay + ".";
+        return prefix(name)+ content(name) + suffix(name);
+
+
+//        if (name != null && name.toUpperCase().equals(name)){
+//            return "HELLO, " + name + "!";
+//        }
+//        String nameToDisplay = name == null ?"my friend" : name;
+//        return "Hello, " + nameToDisplay + ".";
 
 //        if (name == null){
 //           return "Hello, my friend.";
         }
 
+    private String prefix(String name) {
+        return isCapitalizedName(name) ? "HELLO, " : "Hello, ";
     }
+
+    private String content(String name) {
+        if (StringUtils.isBlank(name)){
+            return "my firend";
+        }
+        StringBuilder builder = new StringBuilder();
+        String[] names = name.split(",");
+        for (int i = 0; i < names.length - 1; i++) {
+            builder.append(names[i])
+                    .append(getDelimiter(i, names,name));
+//                            isCapitalizedName(name) ? "AND": "amd")
+
+        }
+
+        return builder.append(names[names.length -1]).toString();
+        //return StringUtils.isBlank(name)  ? "my friend" : name;
+    }
+    private String getDelimiter(int index, String[] names, String name){
+        return index != names.length - 2 ? ", " : (
+                isCapitalizedName(name) ? "AND" : " amd ");
+
+    }
+
+    private String suffix(String name) {
+        return isCapitalizedName(name) ? "!" : ".";
+    }
+    private boolean isCapitalizedName(String name){
+        return StringUtils.isNotBlank(name) && name.toUpperCase().equals(name);
+    }
+//    "" -> "Hello, my friend!"
+
+    public static void main(String[]args){
+        WriterService writerService = new WriterService();
+        String write = writerService.write("");
+
+
+        System.out.println(write);
+    }
+}
--- src/test/java/com/sda/service/WriterServiceTest.java
+++ src/test/java/com/sda/service/WriterServiceTest.java
@@ -1,19 +1,22 @@
 package com.sda.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WriterServiceTest {
-    private WriterService writerService;
-    @Before
-    public void init(){
-    this.writerService = new WriterService();
-}
-    @Test
-    public void testOneName(){
-        //given
 
+    private WriterService writerService;
+
+    @Before
+    public void init() {
+        this.writerService = new WriterService();
+    }
+
+    @Test
+    public void testOneName() {
+        //given
         String name = "Szymon";
 
         //when
@@ -21,16 +24,67 @@ public class WriterServiceTest {
 
         //then
         Assert.assertEquals("Hello, Szymon.", result);
+    }
+
+    @Test
+    public void testNullName() {
+        //given
+        String name = null;
+
+        //when
+        String result = writerService.write(name);
+
+        //then
+        Assert.assertEquals("Hello, my friend.", result);
+    }
+
+    @Test
+    public void testBlankName() {
+        //given
+        String name = "";
+
+        //when
+        String result = writerService.write(name);
+
+        //then
+        Assert.assertEquals("Hello, my friend.", result);
+    }
+
+    @Test
+    public void testEmptyName() {
+        //given
+        String name = "   ";
+
+        //when
+        String result = writerService.write(name);
+
+        //then
+        Assert.assertEquals("Hello, my friend.", result);
+    }
+
+    @Test
+    public void testCapitalizedName() {
+        //given
+        String name = "SZYMON";
+
+        //when
+        String result = writerService.write(name);
+
+        //then
+        Assert.assertEquals("HELLO, SZYMON!", result);
+
 
     }
     @Test
-    public void testNullName(){
+    public void setMultipleNames(){
         //given
-        String name = null;
+        String value = "SZYMON,ANNA,JAN";
+
         //when
-        String result = writerService.write(name);
-        //then
-        Assert.assertEquals("Hello, my frien", result);
+        String result = writerService.write(value);
+
+        Assert.assertEquals("HELLO,SZYMON,ANNA AND JAN.", result);
+
 
     }
 }
